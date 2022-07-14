94f30e5f92edb1de42f41f080b92fe21597ed369
Geilson <geilsonsrz@gmail.com>
2022-07-14 15:01:04 -0300

    Criacao dos efeitos.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,19 @@
-// Rolagem do SETOR 3
+// Rolagem do SETOR 3 - slick
 $('.sec3-slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     speed: 1500
 });
+
+//Rooly 0.2.1
+const r = rolly({
+    view: document.querySelector('.app'),
+    native: true
+  });
+  
+  r.init();
+  
+  AOS.init({
+    duration: 1500
+  });
